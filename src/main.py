2aa74dfa494a1d91031c5f2bea5eada0395af255
Igor <ig.g.like@gmail.com>
2025-04-11 09:34:45 +0600

    :zap:Добавил кнопку и обработчик для кнопки:zap:

--- src/main.py
+++ src/main.py
@@ -9,14 +9,26 @@ def main(page: ft.Page):
     def change_name(e):
         print(name_input.value)
 
+    # функция, которая будет вызываться при нажатии кнопки
+    def click_button(e):
+        print(name_input.value)
+
     # создание текстового поля
     name_input = ft.TextField(
         label="введите ваше имя",  # текст подсказки
         on_change=change_name,  # функция, которая будет вызываться при изменении значения
     )
 
-    # добавление текстового поля на страницу(окно)
-    page.add(name_input)
+    # создание кнопки
+    button = ft.ElevatedButton(
+        "сохранить",  # текст на кнопке
+        on_click=click_button,  # функция, которая будет вызываться при нажатии
+        color=ft.Colors.PINK,  # цвет текста на кнопке
+        bgcolor=ft.Colors.AMBER,  # цвет фона кнопки
+    )
+
+    # добавление элементов на страницу(окно)
+    page.add(name_input, button)
 
 
 ft.app(main)
