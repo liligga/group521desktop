db59dde87df61511f3f1b1271159887deb454e43
Igor <ig.g.like@gmail.com>
2025-04-14 19:15:47 +0600

    :white_check_mark:Сделал отображение текста и список дел:white_check_mark:

--- src/main.py
+++ src/main.py
@@ -3,32 +3,55 @@ import flet as ft
 
 def main(page: ft.Page):
     # установка заголовка
-    page.title = "Привет мир"
+    page.title = "Приложение для управления списком дел"
+    todo_list = []  # список дел
+
+    title = ft.Text(
+        value="Список дел на день", size=30, weight=ft.FontWeight.BOLD, italic=True
+    )
 
     # функция, которая будет вызываться при изменении значения текстового поля
-    def change_name(e):
-        print(name_input.value)
+    def change_todo(e):
+        # print(todo_input.value)
+        pass
 
     # функция, которая будет вызываться при нажатии кнопки
     def click_button(e):
-        print(name_input.value)
+        print(todo_input.value)
+        new_todo = f"{todo_input.value}, {category_input.value}"  # делаем строку для отображения, состоящий из двух полей
+        new_text = ft.Text(
+            value=new_todo, size=20, color=ft.Colors.PINK
+        )  # создаем текстовый элемент
+        todo_list_area.controls.append(
+            new_text
+        )  # добавляем новый текстовый элемент с новым делом в колонку
+
+        page.update()  # обновляем страницу, обязательно а то не будет работать
 
     # создание текстового поля
-    name_input = ft.TextField(
-        label="введите ваше имя",  # текст подсказки
-        on_change=change_name,  # функция, которая будет вызываться при изменении значения
+    todo_input = ft.TextField(
+        label="Введите что-нибудь",  # текст подсказки
+        on_change=change_todo,  # функция, которая будет вызываться при изменении значения
+    )
+    category_input = ft.TextField(
+        label="Введите категорию",  # текст подсказки
     )
 
     # создание кнопки
-    button = ft.ElevatedButton(
-        "сохранить",  # текст на кнопке
+    add_button = ft.ElevatedButton(
+        "Добавить",  # текст на кнопке
         on_click=click_button,  # функция, которая будет вызываться при нажатии
         color=ft.Colors.PINK,  # цвет текста на кнопке
         bgcolor=ft.Colors.AMBER,  # цвет фона кнопки
     )
 
+    # создание колонки
+    todo_list_area = ft.Column()  # место, где будет отображаться список
+
     # добавление элементов на страницу(окно)
-    page.add(name_input, button)
+    page.add(
+        title, todo_input, category_input, add_button, todo_list_area
+    )  # от того, в каком порядке они тут добавляются, зависит в каком порядке они отображаются
 
 
 ft.app(main)
